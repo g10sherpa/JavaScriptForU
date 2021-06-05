edd839a9c701b3c936357e2ee8714a656cdcb783
gyamzo sherpa <megyamzo@gmail.com>
2021-06-05 08:34:57 +0545

    hello world

--- Extra/01scopesln.js
+++
@@ -1,7 +0,0 @@
-// consoe.log(name);
-var name = "gyamzo";
-
-if (true) {
-  let address = "sherpa";
-}
-console.log(address);
--- Extra/08callback.js
+++
@@ -1,8 +0,0 @@
-//function that is passed to another function 
-//as a parameter is called callback function.
-
-const message = () =>{
-    console.log("after 4 seconds");
-}
-
-setTimeout(message,4000);
